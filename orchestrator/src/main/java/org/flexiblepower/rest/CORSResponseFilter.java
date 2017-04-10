ffcc726e933f3320252bcf439c25f3f99d2cd623
Wilco Wijbrandi <wilco.wijbrandi@tno.nl>
2017-04-10 14:27:38 +0200

    Allow CORS Auth

--- orchestrator/src/main/java/org/flexiblepower/rest/CORSResponseFilter.java
+++ orchestrator/src/main/java/org/flexiblepower/rest/CORSResponseFilter.java
@@ -21,8 +21,7 @@ public class CORSResponseFilter implements ContainerResponseFilter {
 
         headers.add("Access-Control-Allow-Origin", "*");
         headers.add("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT");
-        headers.add("Access-Control-Allow-Headers", "X-Requested-With, Content-Type");
-        headers.add("Access-Control-Request-Headers", "X-Requested-With, accept, content-type");
+        headers.add("Access-Control-Allow-Headers", "X-Requested-With, Content-Type, authorization");
     }
 
 }
