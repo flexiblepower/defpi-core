f725f69afbcfd5e796c5459c8f4e74a73a1d6357
Wilco Wijbrandi <wilco.wijbrandi@tno.nl>
2017-04-10 13:51:04 +0200

    Allow combination CORS and Authentication

--- orchestrator/src/main/java/org/flexiblepower/rest/CORSResponseFilter.java
+++ orchestrator/src/main/java/org/flexiblepower/rest/CORSResponseFilter.java
@@ -22,6 +22,7 @@ public class CORSResponseFilter implements ContainerResponseFilter {
         headers.add("Access-Control-Allow-Origin", "*");
         headers.add("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT");
         headers.add("Access-Control-Allow-Headers", "X-Requested-With, Content-Type");
+        headers.add("Access-Control-Request-Headers", "X-Requested-With, accept, content-type");
     }
 
 }
