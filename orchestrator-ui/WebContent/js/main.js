15cdf6ca35582640c0726416eedc7c3bc879a283
Wilco Wijbrandi <wilco.wijbrandi@tno.nl>
2017-04-18 10:43:30 +0200

    Added all essential js and css from node

--- orchestrator-ui/WebContent/js/main.js
+++ orchestrator-ui/WebContent/js/main.js
@@ -7,26 +7,41 @@ myApp.config(function(RestangularProvider) {
 	var login = 'admin', password = 'admin', token = window.btoa(login + ':'
 			+ password);
 	RestangularProvider.setDefaultHeaders({
-		'Accept': 'application/json',
+		'Accept' : 'application/json',
 		'Authorization' : 'Basic ' + token,
 	});
 });
 
-myApp.config([
-		'NgAdminConfigurationProvider',
-		function(nga) {
-			var admin = nga.application('EF-Pi Orchestrator').baseApiUrl(
-					'http://localhost:8080/');
+myApp
+		.config([
+				'NgAdminConfigurationProvider',
+				function(nga) {
+					var admin = nga.application('EF-Pi Orchestrator')
+							.baseApiUrl('http://localhost:8080/');
 
-			// Entities
-			var user = nga.entity('user');
+					// Entities
+					var user = nga.entity('user');
 
-			user.listView().fields(
-					[ nga.field('id'), nga.field('username'),
-							nga.field('password'), nga.field('admin'),
-							nga.field('email') ]);
+					user.listView().fields(
+							[ nga.field('id').editable(false),
+									nga.field('username').isDetailLink(true),
+									nga.field('email', 'email'),
+									nga.field('password', 'password'),
+									nga.field('admin', 'boolean') ]);
 
-			admin.addEntity(user);
+					user.creationView().fields(user.listView().fields());
+					user.editionView().fields(user.listView().fields());
 
-			nga.configure(admin);
-		} ]);
+					admin.addEntity(user);
+
+					admin
+							.menu(nga
+									.menu()
+									.addChild(
+											nga
+													.menu(user)
+													.icon(
+															'<span class="glyphicon glyphicon-user"></span>')));
+
+					nga.configure(admin);
+				} ]);
