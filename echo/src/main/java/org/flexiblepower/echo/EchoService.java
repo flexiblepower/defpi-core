f78e0caa37d77769712a8e107b346def76543f79
Coen van Leeuwen <coen.vanleeuwen@tno.nl>
2017-05-12 16:59:00 +0200

    According to new service design, todo: generate this

--- echo/src/main/java/org/flexiblepower/echo/EchoService.java
+++ echo/src/main/java/org/flexiblepower/echo/EchoService.java
@@ -3,13 +3,12 @@ package org.flexiblepower.echo;
 import java.io.Serializable;
 import java.util.Properties;
 
-import org.flexiblepower.service.MessageHandlerFactory;
 import org.flexiblepower.service.Service;
 
-public class EchoService implements Service, EchoMessageHandlerFactory {
-
+public class EchoService implements Service {
+	
 	@Override
-	public void resumeFrom(Serializable state) {
+	public void resumeFrom(Object state) {
 		// TODO Auto-generated method stub
 
 	}
@@ -23,7 +22,6 @@ public class EchoService implements Service, EchoMessageHandlerFactory {
 	@Override
 	public void modify(Properties props) {
 		// TODO Auto-generated method stub
-
 	}
 
 	@Override
@@ -35,24 +33,7 @@ public class EchoService implements Service, EchoMessageHandlerFactory {
 	@Override
 	public void terminate() {
 		// TODO Auto-generated method stub
-
 	}
-
-	@Override
-	public MessageHandlerFactory getMessageHandlerFactory() {
-		return this;
-	}
-
-	@Override
-	public EfiMessageHandler onEfiConnection() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public WidgetMessageHandler onWidgetConnection() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
+	
+	
 }
