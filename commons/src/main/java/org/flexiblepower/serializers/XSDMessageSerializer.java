a47c234276e2f2a448df9eb87e2c9b6e7dee0e98
Wilco Wijbrandi <wilco.wijbrandi@tno.nl>
2017-08-22 14:49:07 +0200

    Fixed bug in XSDMessageSerializer

--- commons/src/main/java/org/flexiblepower/serializers/XSDMessageSerializer.java
+++ commons/src/main/java/org/flexiblepower/serializers/XSDMessageSerializer.java
@@ -19,80 +19,80 @@ import org.flexiblepower.exceptions.SerializationException;
 
 public class XSDMessageSerializer implements MessageSerializer<Object> {
 
-    private final Map<String, Class<?>> classes = new HashMap<>();
-    private final DescriptorType type = DescriptorType.XSD;
-    private JAXBContext ctx;
-    private Marshaller marshaller;
-    private Unmarshaller unmarshaller;
+	private final Map<String, Class<?>> classes = new HashMap<>();
+	private final DescriptorType type = DescriptorType.XSD;
+	private JAXBContext ctx;
+	private Marshaller marshaller;
+	private Unmarshaller unmarshaller;
 
-    @Override
-    public void addMessageClass(final Class<?> cls) {
-        this.classes.put(cls.getName(), cls);
-    }
+	@Override
+	public void addMessageClass(final Class<?> cls) {
+		this.classes.put(cls.getName(), cls);
+	}
 
-    @Override
-    public byte[] serialize(final Object object) throws SerializationException {
-        try {
-            final Class<?> cls = this.classes.get(object.getClass().getName());
-            return this.serialize(object, cls);
-        } catch (final JAXBException e) {
-            throw new SerializationException(e);
-        }
-    }
+	@Override
+	public byte[] serialize(final Object object) throws SerializationException {
+		try {
+			final Class<?> cls = this.classes.get(object.getClass().getName());
+			return this.serialize(object, cls);
+		} catch (final JAXBException e) {
+			throw new SerializationException(e);
+		}
+	}
 
-    @Override
-    public Object deserialize(final byte[] data) throws SerializationException {
-        if (this.unmarshaller == null) {
-            try {
-                this.init();
-            } catch (final JAXBException e) {
-                throw new SerializationException(e);
-            }
-        }
+	@Override
+	public Object deserialize(final byte[] data) throws SerializationException {
+		if (this.unmarshaller == null) {
+			try {
+				this.init();
+			} catch (final JAXBException e) {
+				throw new SerializationException(e);
+			}
+		}
 
-        for (final Class<?> cls : this.classes.values()) {
-            try {
-                final JAXBElement<?> elem = this.unmarshaller
-                        .unmarshal(new StreamSource(new ByteArrayInputStream(data)), cls);
-                return elem.getValue();
-            } catch (final JAXBException e) {
-                // Not this class... try again
-            }
-        }
-        throw new SerializationException("Unable to find parser for message");
-    }
+		for (final Class<?> cls : this.classes.values()) {
+			try {
+				final JAXBElement<?> elem = this.unmarshaller
+						.unmarshal(new StreamSource(new ByteArrayInputStream(data)), cls);
+				return elem.getValue();
+			} catch (final JAXBException e) {
+				// Not this class... try again
+			}
+		}
+		throw new SerializationException("Unable to find parser for message");
+	}
 
-    @Override
-    public DescriptorType getType() {
-        return this.type;
-    }
+	@Override
+	public DescriptorType getType() {
+		return this.type;
+	}
 
-    private <T> byte[] serialize(final Object object, final Class<T> cls) throws JAXBException {
-        final ByteArrayOutputStream baos = new ByteArrayOutputStream();
-        if (this.marshaller == null) {
-            this.init();
-        }
+	private <T> byte[] serialize(final Object object, final Class<T> cls) throws JAXBException {
+		final ByteArrayOutputStream baos = new ByteArrayOutputStream();
+		if (this.marshaller == null) {
+			this.init();
+		}
 
-        if (cls.isAnnotationPresent(XmlRootElement.class)) {
-            this.marshaller.marshal(object, baos);
-        } else {
-            this.marshaller.marshal(
-                    new JAXBElement<>(new QName("", cls.getAnnotation(XmlType.class).name()), cls, null, (T) object),
-                    baos);
-        }
-        return baos.toByteArray();
-    }
+		if (cls.isAnnotationPresent(XmlRootElement.class)) {
+			this.marshaller.marshal(object, baos);
+		} else {
+			this.marshaller.marshal(
+					new JAXBElement<>(new QName("", cls.getAnnotation(XmlType.class).name()), cls, null, (T) object),
+					baos);
+		}
+		return baos.toByteArray();
+	}
 
-    /**
-     * Lazy initialization to avoid having the constructor throw an exception
-     *
-     * @throws JAXBException
-     */
-    private void init() throws JAXBException {
-        this.ctx = JAXBContext.newInstance((Class[]) this.classes.values().toArray());
-        this.unmarshaller = this.ctx.createUnmarshaller();
-        this.marshaller = this.ctx.createMarshaller();
-
-    }
+	/**
+	 * Lazy initialization to avoid having the constructor throw an exception
+	 *
+	 * @throws JAXBException
+	 */
+	private void init() throws JAXBException {
+		Class<?>[] array = new Class<?>[this.classes.size()];
+		this.ctx = JAXBContext.newInstance(this.classes.values().toArray(array));
+		this.unmarshaller = this.ctx.createUnmarshaller();
+		this.marshaller = this.ctx.createMarshaller();
+	}
 
 }
