a853ee8146b6723caa943f4498a3900c2063664f
Coen van Leeuwen <coen.vanleeuwen@tno.nl>
2017-08-29 10:47:15 +0200

    Fix parsing of parameters

--- service/src/test/java/org/flexiblepower/service/ConfigTest.java
+++ service/src/test/java/org/flexiblepower/service/ConfigTest.java
@@ -58,7 +58,50 @@ public class ConfigTest {
         }
     }
 
+    @Test
+    public void testTypes() {
+        final Map<String, String> valueMap = new HashMap<>();
+        valueMap.put("bool", "True");
+        valueMap.put("char", " ");
+        valueMap.put("byte", "0x7F");
+        valueMap.put("short", "32767");
+        valueMap.put("int", "123");
+        valueMap.put("long", "-100000000");
+        valueMap.put("float", "2e-2");
+        valueMap.put("double", "3.1415926535897932384");
+        valueMap.put("string", "");
+
+        final GeneratedConfig config = ServiceConfig.generateConfig(GeneratedConfig.class, valueMap);
+        Assert.assertEquals(true, config.getBool());
+        Assert.assertEquals(' ', config.getChar());
+        Assert.assertEquals(127, config.getByte());
+        Assert.assertEquals(Short.MAX_VALUE, config.getShort());
+        Assert.assertEquals(123, config.getInt());
+        Assert.assertEquals((long) -1e8, config.getLong());
+        Assert.assertEquals((float) 0.02, config.getFloat(), 1e-3);
+        Assert.assertEquals(Math.PI, config.getDouble(), 1e-10);
+        Assert.assertEquals("", config.getString());
+    }
+
     public static interface GeneratedConfig {
+
+        boolean getBool();
+
+        char getChar();
+
+        byte getByte();
+
+        short getShort();
+
+        int getInt();
+
+        long getLong();
+
+        float getFloat();
+
+        double getDouble();
+
+        String getString();
 
         int getUpdateInterval();
 
