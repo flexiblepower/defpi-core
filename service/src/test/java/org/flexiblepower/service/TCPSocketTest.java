9d2c1c1acf2655cb18580479db794ca66f9b6a82
Coen van Leeuwen <coen.vanleeuwen@tno.nl>
2017-10-12 16:09:55 +0200

    Version bump to 0.2.6

--- service/src/test/java/org/flexiblepower/service/TCPSocketTest.java
+++ service/src/test/java/org/flexiblepower/service/TCPSocketTest.java
@@ -5,8 +5,6 @@
  */
 package org.flexiblepower.service;
 
-import java.io.IOException;
-
 import org.junit.Test;
 
 /**
@@ -19,11 +17,23 @@ import org.junit.Test;
 public class TCPSocketTest {
 
     @Test
-    public void doTest() throws InterruptedException, IOException {
-        final TCPSocket client = TCPSocket.asClient("127.0.0.1", 5000);
-        final TCPSocket server = TCPSocket.asServer(5000);
-        client.send("Test data".getBytes());
-        System.out.println(new String(server.read()));
+    public void doTest() throws Exception {
+        try (
+                final TCPSocket client = TCPSocket.asClient("127.0.0.1", 5000);
+                final TCPSocket server = TCPSocket.asServer(5000)) {
+            client.send("Test data".getBytes());
+            System.out.println(new String(server.read()));
+        }
+    }
+
+    @Test
+    public void doTest2() throws Exception {
+        try (
+                final TCPSocket client = TCPSocket.asClient("127.0.0.1", 5000);
+                final TCPSocket server = TCPSocket.asServer(5000)) {
+            client.send("MORE ! Test data".getBytes());
+            System.out.println(new String(server.read()));
+        }
     }
 
 }
