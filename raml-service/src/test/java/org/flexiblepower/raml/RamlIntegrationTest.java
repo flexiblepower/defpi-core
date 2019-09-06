152fa58d6c5cd65f81ea0ea9efb87ceeb39a1ff9
Coen van Leeuwen <coen.vanleeuwen@tno.nl>
2019-09-06 15:23:53 +0200

    Update file header

--- raml-service/src/test/java/org/flexiblepower/raml/RamlIntegrationTest.java
+++ raml-service/src/test/java/org/flexiblepower/raml/RamlIntegrationTest.java
@@ -1,7 +1,21 @@
-/**
- * File RamlIntegrationTest.java
+/*-
+ * #%L
+ * dEF-Pi API
+ * %%
+ * Copyright (C) 2017 - 2018 Flexible Power Alliance Network
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- * Copyright 2019 FAN
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
  */
 package org.flexiblepower.raml;
 
@@ -62,6 +76,9 @@ public class RamlIntegrationTest {
         Assertions.assertThrows(NullPointerException.class,
                 () -> RamlIntegrationTest.client.getExample()
                         .setStuff(100, "waarde", 25.0, Collections.singletonMap("waarde", null)));
+        Assertions.assertThrows(NumberFormatException.class,
+                () -> RamlIntegrationTest.client.getExample()
+                        .setStuff(100, "waarde", 25.0, Collections.singletonMap("waarde", "zeven")));
     }
 
     @AfterAll
