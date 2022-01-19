b9952194a2974e313cf6d7be981c3a6fcd39bc64
Coen van Leeuwen <coen.vanleeuwen@tno.nl>
2022-01-19 11:07:30 +0100

    Fix XML binding import for newer JDKs

--- commons/src/main/java/org/flexiblepower/serializers/XSDMessageSerializer.java
+++ commons/src/main/java/org/flexiblepower/serializers/XSDMessageSerializer.java
@@ -7,9 +7,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -24,16 +24,17 @@ import java.io.ByteArrayOutputStream;
 import java.util.HashMap;
 import java.util.Map;
 
-import javax.xml.bind.JAXBContext;
-import javax.xml.bind.JAXBElement;
-import javax.xml.bind.JAXBException;
-import javax.xml.bind.Marshaller;
-import javax.xml.bind.Unmarshaller;
-import javax.xml.bind.annotation.XmlRootElement;
-import javax.xml.bind.annotation.XmlType;
 import javax.xml.namespace.QName;
 
 import org.flexiblepower.exceptions.SerializationException;
+
+import jakarta.xml.bind.JAXBContext;
+import jakarta.xml.bind.JAXBElement;
+import jakarta.xml.bind.JAXBException;
+import jakarta.xml.bind.Marshaller;
+import jakarta.xml.bind.Unmarshaller;
+import jakarta.xml.bind.annotation.XmlRootElement;
+import jakarta.xml.bind.annotation.XmlType;
 
 /**
  * XSDMessageSerializer
@@ -113,7 +114,7 @@ public class XSDMessageSerializer implements MessageSerializer<Object> {
      */
     private void init() throws JAXBException {
         final Class<?>[] array = new Class<?>[this.classes.size()];
-        JAXBContext ctx = JAXBContext.newInstance(this.classes.values().toArray(array));
+        final JAXBContext ctx = JAXBContext.newInstance(this.classes.values().toArray(array));
         this.unmarshaller = ctx.createUnmarshaller();
         this.marshaller = ctx.createMarshaller();
     }
